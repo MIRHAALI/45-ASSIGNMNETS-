fdd17c48ddae97f9bf39e49074a0b21ff0e8fb31
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:21:10 +0500

    Add files via upload

---
+++ ass6.js
@@ -0,0 +1,7 @@
+var personName = "\t \n   John Doe \n\t "; // Example name with whitespace characters
+// Print the name with whitespace
+console.log("Name with whitespace:");
+console.log(personName);
+// Print the name after stripping whitespace
+console.log("\nName after stripping whitespace:");
+console.log(personName.trim());
---
+++ ass6.ts
@@ -0,0 +1,9 @@
+let personName: string = "\t \n   John Doe \n\t "; // Example name with whitespace characters
+
+// Print the name with whitespace
+console.log("Name with whitespace:");
+console.log(personName);
+
+// Print the name after stripping whitespace
+console.log("\nName after stripping whitespace:");
+console.log(personName.trim());
