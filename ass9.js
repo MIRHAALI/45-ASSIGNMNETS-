fa360e297dc574797a08ac1026da06cd7eecf07b
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:28:27 +0500

    Add files via upload

---
+++ ass9.js
@@ -0,0 +1,3 @@
+var favoriteNumber = 7; // Replace with your favorite number
+var message = "My favorite number is ".concat(favoriteNumber, ".");
+console.log(message);
---
+++ ass9.ts
@@ -0,0 +1,5 @@
+let favoriteNumber: number = 7; // Replace with your favorite number
+
+let message: string = `My favorite number is ${favoriteNumber}.`;
+
+console.log(message);
