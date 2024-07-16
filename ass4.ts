679956b3e2a6255b85602f789fb7dbe00373f29e
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 18:57:25 +0500

    Add files via upload

---
+++ ass4.ts
@@ -0,0 +1,4 @@
+let Quote: string = "Success is not final, failure is not fatal: It is the courage to continue that counts.";
+let Author: string = "Winston Churchill";
+
+console.log(`"${Quote}" - ${Author}`);
