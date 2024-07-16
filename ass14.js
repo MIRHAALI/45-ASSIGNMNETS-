c08016cf0858aab74c9ad366ebbf3293bbb36402
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:55:56 +0500

    Add files via upload

---
+++ ass14.js
@@ -0,0 +1,8 @@
+var guestList = [
+    "Albert Einstein",
+    "Leonardo da Vinci",
+    "Marie Curie"
+];
+guestList.forEach(function (person) {
+    console.log("Dear ".concat(person, ", you are invited to dinner tonight. Please join us for a delightful evening!"));
+});
---
+++ ass14.ts
@@ -0,0 +1,10 @@
+
+let guestList: string[] = [
+    "Albert Einstein",
+    "Leonardo da Vinci",
+    "Marie Curie"
+];
+
+guestList.forEach(person => {
+    console.log(`Dear ${person}, you are invited to dinner tonight. Please join us for a delightful evening!`);
+});
