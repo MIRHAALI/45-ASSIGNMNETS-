90518d7eb89287c6642995ad6bdd9108d7140177
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 20:54:46 +0500

    Add files via upload

---
+++ ass25.js
@@ -0,0 +1,10 @@
+var alien_color = 'green';
+// Version that passes the if test
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points!");
+}
+var alien_color = 'red';
+// Version that fails the if test (no output)
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points!");
+}
---
+++ ass25.ts
@@ -0,0 +1,14 @@
+let alien_color: string = 'green';
+
+// Version that passes the if test
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points!");
+}
+
+
+let alien_color: string = 'red';
+
+// Version that fails the if test (no output)
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points!");
+}
