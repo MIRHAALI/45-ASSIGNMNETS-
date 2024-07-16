554f3ff7dd38a8165315876f243c3be61192530a
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 20:57:18 +0500

    Add files via upload

---
+++ ass26.ts
@@ -0,0 +1,18 @@
+let alien_color: string = 'green';
+
+// Version that runs the if block
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the green alien!");
+} else {
+    console.log("The player just earned 10 points for shooting the alien!");
+}
+
+
+let alien_color: string = 'red';
+
+// Version that runs the else block
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the green alien!");
+} else {
+    console.log("The player just earned 10 points for shooting the alien!");
+}
