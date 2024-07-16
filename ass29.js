2eec00e4247a37894838e9bca8c4cbab71b4a67a
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 21:06:49 +0500

    Add files via upload

---
+++ ass29.js
@@ -0,0 +1,27 @@
+var favorite_fruits = ['apple', 'banana', 'mango'];
+// Check if 'banana' is in the array
+if (favorite_fruits.includes('banana')) {
+    console.log("You really like bananas!");
+}
+// Check if 'apple' is in the array
+if (favorite_fruits.includes('apple')) {
+    console.log("You really like apples!");
+}
+// Check if 'orange' is in the array
+if (favorite_fruits.includes('orange')) {
+    console.log("You really like oranges!");
+}
+else {
+    console.log("You don't have oranges in your favorite fruits.");
+}
+// Check if 'mango' is in the array
+if (favorite_fruits.includes('mango')) {
+    console.log("You really like mangoes!");
+}
+// Check if 'kiwi' is in the array
+if (favorite_fruits.includes('kiwi')) {
+    console.log("You really like kiwis!");
+}
+else {
+    console.log("You don't have kiwis in your favorite fruits.");
+}
---
+++ ass29.ts
@@ -0,0 +1,30 @@
+let favorite_fruits: string[] = ['apple', 'banana', 'mango'];
+
+// Check if 'banana' is in the array
+if (favorite_fruits.includes('banana')) {
+    console.log("You really like bananas!");
+}
+
+// Check if 'apple' is in the array
+if (favorite_fruits.includes('apple')) {
+    console.log("You really like apples!");
+}
+
+// Check if 'orange' is in the array
+if (favorite_fruits.includes('orange')) {
+    console.log("You really like oranges!");
+} else {
+    console.log("You don't have oranges in your favorite fruits.");
+}
+
+// Check if 'mango' is in the array
+if (favorite_fruits.includes('mango')) {
+    console.log("You really like mangoes!");
+}
+
+// Check if 'kiwi' is in the array
+if (favorite_fruits.includes('kiwi')) {
+    console.log("You really like kiwis!");
+} else {
+    console.log("You don't have kiwis in your favorite fruits.");
+}
