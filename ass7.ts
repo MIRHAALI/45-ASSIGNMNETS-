db5dc4dd728db7bce3d9df328b6aab67dfca9429
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:23:49 +0500

    Add files via upload

---
+++ ass7.ts
@@ -0,0 +1,15 @@
+// Addition
+let additionResult: number = 5 + 3;
+console.log(`5 + 3 = ${additionResult}`);
+
+// Subtraction
+let subtractionResult: number = 10 - 2;
+console.log(`10 - 2 = ${subtractionResult}`);
+
+// Multiplication
+let multiplicationResult: number = 4 * 2;
+console.log(`4 * 2 = ${multiplicationResult}`);
+
+// Division
+let divisionResult: number = 16 / 2;
+console.log(`16 / 2 = ${divisionResult}`);
