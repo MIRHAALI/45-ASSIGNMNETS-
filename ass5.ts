84cc6522ea24b293cac7d44d2f0a2db9ace69efc
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:07:28 +0500

    Add files via upload

---
+++ ass5.ts
@@ -0,0 +1,7 @@
+let Quote = "Success is not final, failure is not fatal: It is the courage to continue that counts.";
+let Author = "Winston Churchill";
+
+let famous_person = Author; // Store the author's name in famous_person variable
+let message = `"${Quote}" - ${famous_person}`;
+
+console.log(message);
