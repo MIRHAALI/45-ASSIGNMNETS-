0555ae8cfa834782c39fb8a44fe0d1193bb2ed97
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 20:30:33 +0500

    Add files via upload

---
+++ ass22.ts
@@ -0,0 +1,12 @@
+// Define an array of fruits
+let fruits: string[] = [
+    "Apple",
+    "Banana",
+    "Orange"
+];
+
+// Attempt to access an element at an invalid index
+console.log(fruits[3]); // This line will produce an error because index 3 does not exist
+
+// Correct the error by accessing a valid index
+console.log(fruits[1]); // Accessing the second element (index 1) which is "Banana"
