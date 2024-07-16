27b50ec86bb28414f4ea28994c1b16586817e75f
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 20:59:26 +0500

    Add files via upload

---
+++ ass27.ts
@@ -0,0 +1,32 @@
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log("The player earned 5 points for shooting the green alien!");
+} else if (alien_color === 'yellow') {
+    console.log("The player earned 10 points for shooting the yellow alien!");
+} else if (alien_color === 'red') {
+    console.log("The player earned 15 points for shooting the red alien!");
+}
+
+
+let alien_color: string = 'yellow';
+
+if (alien_color === 'green') {
+    console.log("The player earned 5 points for shooting the green alien!");
+} else if (alien_color === 'yellow') {
+    console.log("The player earned 10 points for shooting the yellow alien!");
+} else if (alien_color === 'red') {
+    console.log("The player earned 15 points for shooting the red alien!");
+}
+
+
+let alien_color: string = 'red';
+
+if (alien_color === 'green') {
+    console.log("The player earned 5 points for shooting the green alien!");
+} else if (alien_color === 'yellow') {
+    console.log("The player earned 10 points for shooting the yellow alien!");
+} else if (alien_color === 'red') {
+    console.log("The player earned 15 points for shooting the red alien!");
+}
+
