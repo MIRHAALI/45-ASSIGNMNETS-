b0c38c092aeaf0aaab7cf255af452ab11eb22268
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 21:39:38 +0500

    Add files via upload

---
+++ ass39.js
@@ -0,0 +1,7 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+// Calling the function with three city-country pairs and printing the results
+console.log(city_country('Lahore', 'Pakistan'));
+console.log(city_country('New York', 'USA'));
+console.log(city_country('Tokyo', 'Japan'));
