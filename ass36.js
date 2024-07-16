9e51795bd56b9a50bf045689c2bcee1c2a964c7b
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 21:34:52 +0500

    Add files via upload

---
+++ ass36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("The shirt size is ".concat(size, " and it has the message: \"").concat(message, "\" printed on it."));
+}
+// Example usage
+make_shirt('M', 'Hello, World!');
---
+++ ass36.ts
@@ -0,0 +1,7 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size} and it has the message: "${message}" printed on it.`);
+}
+
+// Example usage
+make_shirt('M', 'Hello, World!');
+ 
