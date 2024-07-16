44ccb0f76138519985dec0cce66a8ebe699189f9
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 20:27:04 +0500

    Add files via upload

---
+++ ass20.js
@@ -0,0 +1,11 @@
+// Define an array of mountains
+var mountains = [
+    "Mount Everest",
+    "K2",
+    "Matterhorn",
+    "Kangchenjunga",
+    "Denali"
+];
+// Print the array of mountains
+console.log("List of Mountains:");
+console.log(mountains);
---
+++ ass20.ts
@@ -0,0 +1,12 @@
+// Define an array of mountains
+let mountains: string[] = [
+    "Mount Everest",
+    "K2",
+    "Matterhorn",
+    "Kangchenjunga",
+    "Denali"
+];
+
+// Print the array of mountains
+console.log("List of Mountains:");
+console.log(mountains);
