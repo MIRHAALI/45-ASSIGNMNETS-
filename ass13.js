5c8156d4adf18555a1bf2f136ee465a42cf39d14
syeda mirha ali <121498193+MIRHAALI@users.noreply.github.com>
2024-07-16 19:37:01 +0500

    Add files via upload

---
+++ ass13.js
@@ -0,0 +1,5 @@
+var transportationModes = ["Honda motorcycle", "Tesla car", "Yamaha jet ski"];
+// Print statements about each mode of transportation
+for (var i = 0; i < transportationModes.length; i++) {
+    console.log("I would like to own a ".concat(transportationModes[i], "."));
+}
---
+++ ass13.ts
@@ -0,0 +1,7 @@
+
+let transportationModes: string[] = ["Honda motorcycle", "Tesla car", "Yamaha jet ski"];
+
+// Print statements about each mode of transportation
+for (let i = 0; i < transportationModes.length; i++) {
+    console.log(`I would like to own a ${transportationModes[i]}.`);
+}
